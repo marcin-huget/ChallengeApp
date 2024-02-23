495c18cf5d6d38b17e901a5442769aa53ad1d7f3
marcin-huget <marcin.huget@op.pl>
2024-02-23 16:44:40 +0100

    Zadanie domowe z dnia 4.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var sex = 'K';
+var age = 29;
+
+if (sex == 'K' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == 'M' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
